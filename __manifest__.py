2f3de12016f4b0e0fef6a09e0bbc52b648565fa5
Orihuela03 <145705629+Orihuela03@users.noreply.github.com>
2025-02-06 22:26:35 +0000

    Add icon

--- __manifest__.py
+++ __manifest__.py
@@ -13,6 +13,9 @@
         'views/task_views.xml',
         'views/subtask_views.xml'
     ],
+    'images': [
+        'static/description/icon.png'
+    ],
     'installable': True,
     'application': True,
 }
